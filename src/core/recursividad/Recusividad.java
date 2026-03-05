a0a2c29f89df3969ffe73334a1daaa52e660fae8
miguelx256 <miancasaplay@gmail.com>
2026-03-04 19:44:25 -0500

    proyecto creado

---
+++ src/core/recursividad/Recusividad.java
@@ -0,0 +1,22 @@
+package core.recursividad;
+
+public class Recusividad {
+
+    public static void contar(int n) {
+        if (n == 0) {
+            return;
+        } else {
+            System.out.println(n);
+            contar(n - 1);
+        }
+    }
+
+    public static int factorial(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return n * factorial(n-1);
+        }
+    }
+
+}
